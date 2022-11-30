bfca2d229c349dcfa260f36767502df6e29a9600
eriklicom <91689709+eriklicom@users.noreply.github.com>
2022-11-29 23:21:59 -0300

    update index.js

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 
+
+
+
+
+import React from "react";
+import Heading from "component/Head";
+import Mein from "component/Mein";
+
+
+function HomePage(){
+  return (
+    <div className="">
+      <Heading/>
+      <div className="">
+        <Mein url="{location.href}"/>
+      </div>
+    </div>
+  )
+}
+
+ //   <div className=""></div>
+
+export default HomePage
+
+
+
+
+
+
+
+
+
+
+
+"scripts": {
+  "start": "index.js"
+}
